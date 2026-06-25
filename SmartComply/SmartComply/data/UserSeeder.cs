324a256c5eea2d914a7e5844266756a9b4661909
Daniel Hakim <129138344+akim730@users.noreply.github.com>
2026-06-25 11:36:13 +0800

    Update UserSeeder.cs

--- SmartComply/SmartComply/data/UserSeeder.cs
+++ SmartComply/SmartComply/data/UserSeeder.cs
@@ -1,7 +1,5 @@
 using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.Configuration;
 using SmartComply.Constant;
-
 namespace SmartComply.data
 {
     public class UserSeeder
@@ -9,43 +7,12 @@ namespace SmartComply.data
         public static async Task SeedUserAsync(IServiceProvider serviceProvider)
         {
             var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
-            var config = serviceProvider.GetRequiredService<IConfiguration>();
-
-            var users = new[]
-            {
-                (
-                    Email: config["SeedUsers:SuperAdmin:Email"]!,
-                    Password: config["SeedUsers:SuperAdmin:Password"]!,
-                    Role: Roles.superAdmin
-                ),
-                (
-                    Email: config["SeedUsers:Admin:Email"]!,
-                    Password: config["SeedUsers:Admin:Password"]!,
-                    Role: Roles.Admin
-                ),
-                (
-                    Email: config["SeedUsers:Manager:Email"]!,
-                    Password: config["SeedUsers:Manager:Password"]!,
-                    Role: Roles.Manager
-                ),
-                (
-                    Email: config["SeedUsers:Auditor:Email"]!,
-                    Password: config["SeedUsers:Auditor:Password"]!,
-                    Role: Roles.Auditor
-                ),
-            };
-
-            foreach (var (email, password, role) in users)
-            {
-                await CreateUserWithRole(userManager, email, password, role);
-            }
+            await CreateUserWithRole(userManager, "superAdmin@gmail.com", "superAdmin123!", Roles.superAdmin);
+            await CreateUserWithRole(userManager, "admin@gmail.com", "Admin1231", Roles.Admin);
+            await CreateUserWithRole(userManager, "manager@gmail.com", "Manager123!", Roles.Manager);
+            await CreateUserWithRole(userManager, "auditor@gmail.com", "Auditor123!", Roles.Auditor);
         }
-
-        private static async Task CreateUserWithRole(
-            UserManager<IdentityUser> userManager,
-            string email,
-            string password,
-            string role)
+        private static async Task CreateUserWithRole(UserManager<IdentityUser> userManager, string email, string password, string role)
         {
             if (await userManager.FindByEmailAsync(email) == null)
             {
@@ -55,18 +22,14 @@ namespace SmartComply.data
                     EmailConfirmed = true,
                     UserName = email
                 };
-
                 var result = await userManager.CreateAsync(user, password);
-
                 if (result.Succeeded)
                 {
                     await userManager.AddToRoleAsync(user, role);
                 }
                 else
                 {
-                    throw new Exception(
-                        $"Failed creating user {email}. Errors: {string.Join(", ", result.Errors.Select(e => e.Description))}"
-                    );
+                    throw new Exception($"Failes creating user with {user.Email}. Error: {string.Join(",", result.Errors)}");
                 }
             }
         }
